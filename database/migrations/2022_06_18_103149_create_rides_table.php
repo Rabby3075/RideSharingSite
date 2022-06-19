5dfc4c0aae094af33df640c2381e9f69db3297cc
Rabby3075 <rashed.rabby43@gmail.com>
2022-06-20 02:17:59 +0600

    rider,ride migration seeding

--- database/migrations/2022_06_18_103149_create_rides_table.php
+++ database/migrations/2022_06_18_103149_create_rides_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
+
 return new class extends Migration
 {
     /**
@@ -15,6 +17,22 @@ return new class extends Migration
     {
         Schema::create('rides', function (Blueprint $table) {
             $table->id();
+            $table->string('customerName');
+            $table->unsignedBigInteger('customerId');
+            $table->string('customerPhone');
+            $table->string('pickupPoint');
+            $table->string('destination');
+            $table->string('length');
+            $table->string('cost');
+            $table->unsignedBigInteger('riderId')->nullable();
+            $table->string('riderName')->nullable();
+            $table->string('riderPhone')->nullable();
+            $table->string('customerStatus')->nullable();
+            $table->string('riderStatus')->nullable();
+
+            $table->foreign('customerId')->references('id')->on('customers');
+            $table->foreign('riderId')->references('id')->on('riders');
+
             $table->timestamps();
         });
     }
