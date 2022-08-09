a9a720c3b701dbc0f4a9f4a2d835372be5394c8f
Rabby3075 <rashed.rabby43@gmail.com>
2022-08-09 23:26:43 +0600

    ride history visible

--- react-site/src/components/CustomerComponents/Dashboard/RideHistory/RideHistory.js
+++ react-site/src/components/CustomerComponents/Dashboard/RideHistory/RideHistory.js
@@ -54,6 +54,30 @@ const RideHistory = () =>{
 
     }
 
+    const showRide = (id)=>{
+        console.log(id);
+        var obj = {id: id};
+        axios.post("http://127.0.0.1:8000/api/rideInformation",obj)
+        .then(resp=>{
+            var response = resp.data;
+            console.log(response);
+            document.getElementById('riderName').innerHTML = response.riderName;
+            document.getElementById('riderPhone').innerHTML = response.riderPhone;
+            document.getElementById('pickPoint').innerHTML=response.pickupPoint;
+            document.getElementById('destination').innerHTML=response.destination;
+            document.getElementById('distance').innerHTML=response.length;
+            document.getElementById('cost').innerHTML=response.cost;
+            document.getElementById('rideReqTime').innerHTML=response.rideRequestTime;
+            document.getElementById('riderAppTime').innerHTML=response.riderApprovalTime;
+            document.getElementById('depTime').innerHTML=response.riderStartingTie;
+            document.getElementById('arrTime').innerHTML=response.reachedTime;
+
+
+        }).catch(err=>{
+            console.log(err);
+        });
+    }
+
 
 
     return(
@@ -118,7 +142,7 @@ const RideHistory = () =>{
                 ride.customerStatus === 'Waiting for rider...' &&<button className="btn btn-danger text-white" onClick={(e)=>showCancel(ride.id)} data-bs-toggle="modal" data-bs-target="#cancelModal"><i className="bi bi-x-circle-fill"></i> Cancel Ride</button>
             }
             {
-                ride.customerStatus === 'Ride complete' &&<a href="javascript:void(0)" className="btn btn-primary"  id="viewRide" data-url="{{ route('rideView', $ride->id) }}"><i className="bi bi-eye me-2 text-white"></i>View Details</a>
+                ride.customerStatus === 'Ride complete' &&<button className="btn btn-primary"  onClick={(e)=>showRide(ride.id)} data-bs-toggle="modal" data-bs-target="#detailsModal"><i className="bi bi-eye me-2 text-white"></i>View Details</button>
             }
             {
                 ride.customerStatus === 'Cancel' &&<p className="text-danger"> Ride Cancel at <strong>{ride.cancelTime}</strong></p>
@@ -144,6 +168,7 @@ const RideHistory = () =>{
 
 </div>
             <Footer />
+            {/*----------------Cancel modal -----------------*/}
 <div className="modal fade" id="cancelModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
@@ -165,6 +190,51 @@ const RideHistory = () =>{
     </div>
   </div>
 </div>
+{/*----------------Details modal -----------------*/}
+<div className="modal fade" id="detailsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h3 className="modal-title text-primary">Ride Information</h3>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+
+        <input type="hidden" id="rideId" name="rideid" />
+
+        <h4 className="d-flex justify-content-center">Rider Information</h4>
+        <div className="text-center">
+         <p>Rider Name: <span className="riderName" id="riderName"></span></p>
+         <p>Rider Phone Number: <span className="riderPhone" id="riderPhone"></span></p>
+        </div>
+
+        <h4 className="d-flex justify-content-center">Ride Information</h4>
+        <div className="row text-center">
+        <div className="col">
+        <p>Pick Point: <span className="pickPoint" id="pickPoint"></span></p>
+        <p>Destination: <span className="destination" id="destination"></span></p>
+        <p>Total Distance: <span className="distance" id="distance"></span> Kilo</p>
+        <p>Cost: <span className="cost" id="cost"></span> TK</p>
+        </div>
+        <div className="col">
+        <p>Ride Request Time: <span className="rideReqTime" id="rideReqTime"></span></p>
+        <p>Rider Approval Time: <span className="riderAppTime" id="riderAppTime"></span></p>
+        <p>Departure Time: <span className="depTime" id="depTime"></span></p>
+        <p>Arrival Time:  <span className="arrTime" id="arrTime"></span></p>
+
+        </div>
+        </div>
+
+
+
+        <textarea className="form-control border border-primary" id="msg" name = "msg" rows="3" placeholder="Please type your review message"></textarea>
+        <input type="submit" className="btn btn-outline-primary mt-2 text-center" id="review" value="Submit your review" />
+
+        </div>
+    </div>
+  </div>
+</div>
+
 
         </div>
 
